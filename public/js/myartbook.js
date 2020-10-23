0b0f07c9556649bac62932c1b9436a1eac724bc2
JDBgit9 <info@jamesdbeeks.com>
2020-10-23 19:41:15 -0400

    added the env file, added pages to the book

--- public/js/myartbook.js
+++ public/js/myartbook.js
@@ -1,1 +1,20 @@
-$('#flipbook').turn({gradients: true, acceleration: true});
+// var saveButton = document.getElementById("saveBtn");
+
+// $('#flipbook').turn({gradients: true, acceleration: true});
+
+// $(document).on("click", ".img-c.active", function() {
+//     let copy = $(this)
+//     copy.removeClass("positioned active").addClass("postactive")
+//     setTimeout(function() {
+//     copy.remove();
+//     }, 500)
+//     })
+    // $("#pageFld").val($("#flipbook").turn("page"));
+
+    // $("#flipbook").bind("turned", function (event, page, view) {
+    //     $("#pageFld").val(page);
+    // });
+
+    // $("#pageFld").change(function () {
+    //     $("#flipbook").turn("page", $(this).val());
+    // });
