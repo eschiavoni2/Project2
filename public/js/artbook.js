f4077c64272598051fe056e4bafe34b9569daa5c
JDBgit9 <info@jamesdbeeks.com>
2020-10-24 19:32:58 -0400

    updated password to connect to db

--- public/js/artbook.js
+++ public/js/artbook.js
@@ -1,16 +1,16 @@
-// var mysql = require("mysql");
-// var inquirer = require("inquirer");
-// // create the connection information for the sql database
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   // Your port; if not 3306
-//   port: 3306,
-//   // Your username
-//   user: "root",
-//   // Your password
-//   password: "root",
-//   database: "plants"
-// });
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  // Your port; if not 3306
+  port: 3306,
+  // Your username
+  user: "root",
+  // Your password
+  password: "root",
+  database: "plants"
+});
 
 
 // $('#flipbook').turn({gradients: true, acceleration: true});
