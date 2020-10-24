3fc1870f5f504e210823046a1a417dc4abe9d7ab
JDBgit9 <info@jamesdbeeks.com>
2020-10-24 13:26:43 -0400

    added text to the artbook.js file and commented out for now

--- public/js/artbook.js
+++ public/js/artbook.js
@@ -1,1 +1,36 @@
-$('#flipbook').turn({gradients: true, acceleration: true});
+// $('#flipbook').turn({gradients: true, acceleration: true});
+
+// let saveBtn = document.querySelector("button").addEventListener("change", function (){
+//     console.log(this.user_data)
+// })
+// let artImage = document.querySelector('savedArt');
+
+// saveBtn.addEventListener('click', () => {
+//     let artImage = img.getAttribute('src');
+//     let artbook = getFileName(imgPath);
+
+//     saveAs(imgPath, fileName)
+
+// });
+
+// function getFileName(str){
+//     return str.substring(str.lastIndexOf('/')+1);
+// }
+
+// $(document).on("click", ".img-c.active", function() {
+//     let copy = $(this)
+//     copy.removeClass("positioned active").addClass("postactive")
+//     setTimeout(function() {
+//     copy.remove();
+//     }, 500)
+//     })
+//     $("#pageFld").val($("#flipbook").turn("page"));
+
+//     $("#flipbook").bind("turned", function (event, page, view) {
+//         $("#pageFld").val(page);
+//     });
+
+//     $("#pageFld").change(function () {
+//         $("#flipbook").turn("page", $(this).val());
+//     });
+
