0fafb8e11a283ff03e9a0a063300b74bb66470a2
JDBgit9 <info@jamesdbeeks.com>
2020-10-24 18:21:08 -0400

    created the syntax for the db connection

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
 "development": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
-    "database": "f47qfgaxx78y6jn2",
+    "database": "plants",
     "host": process.env.DB_HOST,
     "dialect": "mysql"
 },
