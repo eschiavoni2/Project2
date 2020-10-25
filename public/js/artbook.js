6a1c1c2440cbac747109f11267b78f771f804a3b
John Nederveld <john.nederveld@gmail.com>
2020-10-24 21:25:58 -0400

    fixed js

--- public/js/artbook.js
+++ public/js/artbook.js
@@ -1,1 +1,12 @@
-$('#flipbook').turn({gradients: true, acceleration: true});
+
+let member;
+
+$(document).ready(() => {
+    // This file just does a GET request to figure out which user is logged in
+    // and updates the HTML on the page
+    $.get("/api/user_data").then(data => {
+      $(".member-name").text(data.email);
+      member = data.email;
+    });
+    console.log(member);
+  });
